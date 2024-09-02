71e2bc4185f435e98ca73a320f6a1f9840e50887
Radim Černil <Nabis1@users.noreply.github.com>
2024-09-02 21:19:16 +0200

    refactor: Use descriptive variable names for page objects
    - Replaced 'hp' with 'home_page' and 'cp' with 'course_page' in all tests.
    - Updated test files to improve code readability and maintainability.

--- tests/test_engeto.py
+++ tests/test_engeto.py
@@ -9,44 +9,44 @@ from pages.course_page import CoursePage
 @pytest.mark.playwright
 def test_to_click_course_date(page: Page):
 
-    hp = HomePage(page)
-    cp = CoursePage(page)
+    home_page = HomePage(page)
+    course_page = CoursePage(page)
 
-    hp.navigate()
-    hp.accept_cookies()
-    cp.click_specific_button_inside_nav_bar('Termíny')
+    home_page.navigate()
+    home_page.accept_cookies()
+    course_page.click_specific_button_inside_nav_bar('Termíny')
     CoursePage(page).assert_page_url('https://engeto.cz/terminy/')
 
 @pytest.mark.playwright
 def test_click_image(page: Page):
     
-    hp = HomePage(page)
-    cp = CoursePage(page)
+    home_page = HomePage(page)
+    course_page = CoursePage(page)
 
-    hp.navigate()
-    hp.accept_cookies()
-    cp.scroll_down(400, 1000)
-    cp.click_specific_banner('Tester s Pythonem')
-    cp.assert_page_url('https://engeto.cz/tester-s-pythonem/')
+    home_page.navigate()
+    home_page.accept_cookies()
+    course_page.scroll_down(400, 1000)
+    course_page.click_specific_banner('Tester s Pythonem')
+    course_page.assert_page_url('https://engeto.cz/tester-s-pythonem/')
     
 @pytest.mark.playwright
 def test_add_course_to_cart(page: Page):
 
-    hp = HomePage(page)
-    cp = CoursePage(page)
+    home_page = HomePage(page)
+    course_page = CoursePage(page)
 
 
-    hp.navigate()
-    hp.accept_cookies()
-    cp.scroll_down(400, 1000)
-    cp.click_specific_banner('Testing Akademie')
-    cp.assert_page_url('https://engeto.cz/testovani-softwaru/')
+    home_page.navigate()
+    home_page.accept_cookies()
+    course_page.scroll_down(400, 1000)
+    course_page.click_specific_banner('Testing Akademie')
+    course_page.assert_page_url('https://engeto.cz/testovani-softwaru/')
 
-    cp.click_specific_text_in_product_box('10. 12. – 11. 03. 2025, Online', 'Detail Termínu')
-    cp.click_specific_text("Přihlas se na termín")
-    cp.click_specific_text('Přejít k pokladně')
+    course_page.click_specific_text_in_product_box('10. 12. – 11. 03. 2025, Online', 'Detail Termínu')
+    course_page.click_specific_text("Přihlas se na termín")
+    course_page.click_specific_text('Přejít k pokladně')
 
-    cp.fill_billing_info(
+    course_page.fill_billing_info(
         first_name="Karel",
         last_name="Čtvrtý",
         phone="777 999 999",
@@ -56,5 +56,5 @@ def test_add_course_to_cart(page: Page):
         postcode="267 18"
     )
 
-    cp.agree_to_terms()
-    cp.submit_order()
+    course_page.agree_to_terms()
+    course_page.submit_order()
